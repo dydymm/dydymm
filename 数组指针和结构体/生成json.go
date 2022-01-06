a816f83fcd3e01e6e2bd58177e34063224091c5d
dydymm <851481507@qq.com>
2022-01-06 13:53:01 +0800

    json生成

--- 数组指针和结构体/生成json.go
+++ 数组指针和结构体/生成json.go
@@ -5,9 +5,29 @@ import (
 	"fmt"
 )
 
-type Code struct {
+type Result struct {
+	Code	int		`json:"code"`
+	Message	string	`json:"msg"`
 }
 
 func main() {
+	//指针变量指向了一个值的内存地址。
+	var res Result
+	res.Code	=200
+	res.Message="success"
+
+	//序列化
+	jsons,errs:=json.Marshal(res)
+	if errs!=nil{
+		fmt.Println("json marshal error:",errs)
+	}
+	fmt.Println("json data :",string(jsons))
+	//反序列化
+	var res2 Result
+	errs = json.Unmarshal(jsons,&res2)
+	if errs !=nil{
+		fmt.Println("json unmarshal error :",errs)
+	}
+	fmt.Println("res2 :",res2)
 
 }
