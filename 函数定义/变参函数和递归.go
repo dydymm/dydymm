b9c6c7fad7dc82e7846f835cb6f0b639d167b8da
dydymm <851481507@qq.com>
2022-01-07 10:58:54 +0800

    master

--- 函数定义/变参函数和递归.go
+++ 函数定义/变参函数和递归.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func sum(nums ...int) {
+func sum(nums ...int) { //变参函数
 	fmt.Print(nums, "")
 	total := 1
 	for _, num := range nums {
@@ -11,10 +11,20 @@ func sum(nums ...int) {
 	fmt.Println(total)
 }
 
+func fact(n int) int { //递归
+	if n == 0 {
+		return 1
+	}
+	return n * fact(n-1)
+}
+
 func main() {
 	sum(1, 2)
 	sum(1, 2, 3)
 
 	nums := []int{1, 2, 3, 4, 5}
 	sum(nums...)
+
+	//递归
+	fmt.Println(fact(7))
 }
